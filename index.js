84f726adef155e4fb96ab53d2f9720531af93200
generaal233 <generaal123@gmail.com>
2020-07-09 10:59:23 +0200

    2

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
 
-
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
@@ -46,38 +45,19 @@ const tech = io.of('/tech');
 
 tech.on('connection', (socket) => {
     socket.on('join', (data) => {
-        socket.join(data.room);
-
-        db.getChats(data.room).then( val => {
-            // console.log(val);
-            tech.to(socket.id).emit('historyChats',val);
-
-            tech.in(data.room).emit('singleMessage', `${data.user} joined ${data.room} room!`);
-        });
-
-        db.checkUser(data.user).then(value => {
-            if(value == true){
-                db.insertUser();
-            }
-        });
-
-       
-    });
+        socket.join(data.room)
+        tech.in(data.room).emit('message', `New user joined ${data.room} room!`)
+    })
 
     socket.on('message', (data) => {
-        console.log(`message ${data.msg}`);
-
-        var message = {
-            user: data.user,
-            room: data.room,
-            msg: data.msg
-        };
-
-        let insert = db.insertChats(message);
-        tech.in(data.room).emit('message', message);
+        console.log('message: ${data.msg}');
+        tech.in(data.room).emit('message', data.msg);
     });
 
     socket.on('disconnect', () => {
-        tech.emit('singleMessage', 'user disconnected');
+        console.log('user disconnected');
+
+        tech.emit('message', 'user disconnected');
     })
-});
+    
+})
